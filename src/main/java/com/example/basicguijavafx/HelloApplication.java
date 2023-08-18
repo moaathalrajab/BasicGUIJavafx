bc2e2b2c055a610e5bd6780aebeeb944435b222f
Moaath Alrajab <moaath@alrajab.net>
2023-08-18 19:31:36 -0400

    3 different Start methods

--- src/main/java/com/example/basicguijavafx/HelloApplication.java
+++ src/main/java/com/example/basicguijavafx/HelloApplication.java
@@ -18,12 +18,32 @@ public class HelloApplication extends Application {
 //        stage.show();
 //    }
 
-
+/*    @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
+        // Create a scene and place a button in the scene
+        Scene scene = new Scene(new Button("OK"), 200, 250);
+        primaryStage.setTitle("First Demo"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        Stage stage = new Stage(); // Create a new stage
+        stage.setTitle("Second Stage"); // Set the stage title
+        // Set a scene with a button in the stage
+        stage.setScene(new Scene(new Button("New Stage"), 200, 250));
+        stage.show(); // Display the stage
+    }*/
+
+   public void start(Stage primaryStage) {
         // Create a button and place it in the scene
         Button btOK = new Button("OK");
+        btOK.setOnAction(e->{
+            Stage stage = new Stage(); // Create a new stage
+            stage.setTitle("Second Stage"); // Set the stage title
+            // Set a scene with a button in the stage
+            primaryStage.setScene(new Scene(new Button("New Stage"), 400, 450));
+        });
         Scene scene = new Scene(btOK, 200, 250);
-        primaryStage.setTitle("MyJavaFX"); // Set the stage title
+        primaryStage.setTitle("First Demo"); // Set the stage title
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
     }
